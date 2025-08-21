6a3fe2cb02ee29400417c287b186d42b5135c740
Addiel Lucena <addiellucena94@gmail.com>
2025-08-21 14:33:47 -0300

    The dimension for the header was fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
 const Header = () => {
+    /*
+    <li><a href="/" className="hover:text-blue-500 transition">Home</a></li>
+    <li><a href="/schedule" className="hover:text-blue-500 transition">Schedule</a></li>
+    <li><a href="/sop" className="hover:text-blue-500 transition">SOP</a></li>
+    <li><a href="/workers" className="hover:text-blue-500 transition">Workers</a></li>
+    <li><a href="/rules" className="hover:text-blue-500 transition">Internal Rules</a></li>
+    <li><a href="documents" className="hover:text-blue-500 transition">Documents</a></li>
+    */
+
     return (
-        <>
-            <header className="bg-white shadow-md">
-                <nav className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
-                    <div className="text-2xl font-bold text-blue-500">Infotrack Solutions</div>
-                    <ul className="flex space-x-8 text-gray-700 text-base font-medium">
-                        <li><a href="/" className="hover:text-blue-500 transition">Home</a></li>
-                        <li><a href="/schedule" className="hover:text-blue-500 transition">Schedule</a></li>
-                        <li><a href="/sop" className="hover:text-blue-500 transition">SOP</a></li>
-                        <li><a href="/workers" className="hover:text-blue-500 transition">Workers</a></li>
-                        <li><a href="/rules" className="hover:text-blue-500 transition">Internal Rules</a></li>
-                        <li><a href="documents" className="hover:text-blue-500 transition">Documents</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </>
+        <header className="bg-white shadow-md">
+            <nav className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+                <div className="text-2xl font-bold text-blue-500">Infotrack Solutions</div>
+                <ul className="flex space-x-8 text-gray-700 text-base font-medium">
+                    <button>
+                        Sign In
+                    </button>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
